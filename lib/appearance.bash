8b4d3317f3b466e11a43310f16f4bb2af547ea86
Zach Riggle <zach@riggle.me>
2013-05-26 21:18:31 -0400

    Msysgit grep does not support color

--- lib/appearance.bash
+++ lib/appearance.bash
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # colored grep
-export GREP_OPTIONS='--color=auto'
-export GREP_COLOR='1;33'
+#export GREP_OPTIONS='--color=auto'
+#export GREP_COLOR='1;33'
 
 # colored ls
 export LSCOLORS='Gxfxcxdxdxegedabagacad'
